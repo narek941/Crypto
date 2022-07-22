90b15ff0c39ec60aeeb5624bf14b5634f5fcf821
Hrant Abrahamyan <hra@unicsoft.com>
2022-07-22 17:33:40 +0400

    create analytics page's tabs, create Trade's table with real data

---
+++ src/components/shared/TradesTable/TradesTableRow.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import moment from 'moment';
+import { v4 as uuid4 } from 'uuid';
+
+import styles from './TradesTable.module.scss';
+
+const TradesTableRow = ({ row }: any) => {
+  return (
+    <React.Fragment>
+      <TableRow className={styles.container__body__row}>
+        <>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {moment(row.createdAt).format('MM.DD.YY')}
+          </TableCell>
+
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row?.coinsPair?.name}
+          </TableCell>
+
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.side}
+          </TableCell>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.price}
+          </TableCell>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.amount}
+          </TableCell>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.value}
+          </TableCell>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.totalPrice || 0}
+          </TableCell>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.totalPriceInBaseCurrency || 0}
+          </TableCell>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.fees || 0}
+          </TableCell>
+          <TableCell align='left' className={styles.ceil} key={uuid4()}>
+            {row.feesInBaseCurrency || 0}
+          </TableCell>
+        </>
+      </TableRow>
+    </React.Fragment>
+  );
+};
+
+export default TradesTableRow;
--- src/containers/AccountsAnalytics/index.tsx
+++ src/containers/AccountsAnalytics/index.tsx
@@ -1,46 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
 import { isUndefined } from 'lodash';
 
 import { useAppDispatch } from 'hooks';
-import { Bricks, Chart, OrdersTable, Doughnut, Export } from 'components';
+import { Bricks, Chart, Doughnut, Export } from 'components';
 import { accountsAnalyticsLineChart } from 'utils/table';
 import { accountsActions } from 'store/accountsSlice';
 import { RootState } from 'types';
-import { accountsFilterUpdate } from 'store/accountsSlice/thunks';
-import { KeyOfData } from 'components/views/Table/types';
+import AnalyticsTabs from 'components/views/AnalyticsTabs';
 
 import styles from './AccountsAnalytics.module.scss';
 
 const AccountsAnalytics: React.FC = () => {
-  const [page, setPage] = useState(0);
   const dispatch = useAppDispatch();
   const { id } = useParams();
   const convertedId = Number(id);
-  const [orderBy, setOrderBy] = useState<KeyOfData>('id');
 
-  const { accountsFilter, accountById } = useSelector((state: RootState) => state.accounts);
-
-  const handleRequestSort = (event: React.MouseEvent<unknown>, property: KeyOfData) => {
-    const isAsc = orderBy === property && accountsFilter.order === 'ASC';
-    const orderText = isAsc ? 'DESC' : 'ASC';
-
-    dispatch(accountsFilterUpdate({ order: orderText }));
-
-    setOrderBy(property);
-  };
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    dispatch(accountsFilterUpdate({ skip: Number(newPage) * accountsFilter.take }));
-
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(accountsFilterUpdate({ take: parseInt(event.target.value), skip: 0 }));
-  };
+  const { accountById } = useSelector((state: RootState) => state.accounts);
 
   const renderLineCharts = accountsAnalyticsLineChart.map((item: any, index) => (
     <Chart key={index} />
@@ -51,19 +29,13 @@ const AccountsAnalytics: React.FC = () => {
   ));
 
   useEffect(() => {
-    dispatch(accountsActions.getWalletOpenOrders({ ...accountsFilter, id: id as string }));
-  }, [id, accountsFilter, dispatch]);
-
-  useEffect(() => {
     dispatch(accountsActions.getAccountSummary(convertedId));
     dispatch(accountsActions.getAccountById(convertedId));
 
     return () => {
       dispatch(accountsActions.removeAccountById());
     };
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [convertedId, dispatch]);
 
   return (
     <div className={styles.analytics}>
@@ -102,12 +74,7 @@ const AccountsAnalytics: React.FC = () => {
       </div>
       <div className={styles.analytics__chart}>{renderLineCharts}</div>
       <div className={styles.analytics__chart}>{renderDoughnutCharts}</div>
-      <OrdersTable
-        handleRequestSort={handleRequestSort}
-        handleChangePage={handleChangePage}
-        handleChangeRowsPerPage={handleChangeRowsPerPage}
-        page={page}
-      />
+      <AnalyticsTabs />
     </div>
   );
 };
